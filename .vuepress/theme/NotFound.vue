5fa26ab1aa38168fcc9c48dce49455b22c0c62b3
bit-business <101217799+bit-business@users.noreply.github.com>
2022-03-25 01:45:37 +0100

    izmjena2

--- .vuepress/theme/NotFound.vue
+++ .vuepress/theme/NotFound.vue
@@ -7,7 +7,7 @@
                         <b-card class="shadow-lg" bg-variant="light">
                             <h1>404</h1>
                             <blockquote>{{ getMsg() }}</blockquote>
-                            <router-link href="https://crypto-studio.net/">Take me home</router-link>.
+                            <router-link to="https://crypto-studio.net/">Take me home</router-link>.
                         </b-card>
                     </transition>
                 </b-col>
