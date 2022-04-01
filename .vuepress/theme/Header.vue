574d2ac26802e6e1b2ded317428f2a9e5a7b7c10
bit-business <101217799+bit-business@users.noreply.github.com>
2022-04-01 17:15:55 +0200

    connect

--- .vuepress/theme/Header.vue
+++ .vuepress/theme/Header.vue
@@ -20,20 +20,85 @@
       </template>
      Creator for ETHEREUM network is nearly finished. If you need it, please contact us by contact form and we will send you a link to BETA version.
     </b-toast>
-       <b-navbar-brand>
-        </b-navbar-brand><button disabled class="btn btn-dark"></button>
-        </b-navbar-nav>
+       </b-navbar-nav>  <b-navbar-nav>
+<b-nav-item> <button type="button" class="btn btn-outline-warning" @click="connectmetamaskbutton" ref="btnToggle" >
+
+                                    Connect</button>
+  </b-nav-item> </b-navbar-nav>
     </b-navbar>
 </template>
 
 <script>
+  import dapp from '../mixins/dapp';
   export default {
     name: 'Header',
+    mixins: [
+      dapp,
+    ],
     props: {
       page: {
         type: String,
         default: null,
       },
     },
+
+    mounted () {
+      this.currentNetwork = this.getParam('network') || this.network.default;
+      this.initDapp();
+    },
+    methods: {
+      async initDapp () {
+        this.network.current = this.network.list[this.currentNetwork];
+        try {
+          await this.initWeb3(this.currentNetwork, true);
+          this.initService(this.currentNetwork);
+        } catch (e) {
+          console.log(e); // eslint-disable-line no-console
+          this.makeToast(
+            'Some errors occurred',
+            e,
+            'danger',
+          );
+          // document.location.href = this.$withBase('/');
+        }
+      },
+
+      async connectmetamaskbutton () {
+        try {
+          await this.web3Provider.request({ method: 'eth_requestAccounts' });
+        } catch (e) {
+          this.show = !this.show;
+          this.$refs.btnToggle.innerText = 'Connect';
+          this.$refs.btnToggle.className = 'btn btn-outline-warning';
+        }
+
+        ethereum.on('chainChanged', () => {
+          document.location.reload();
+        });
+
+        if (!this.metamask.installed) {
+          this.makeToast(
+            'No Wallet',
+            'To create a Token you need to install MetaMask!',
+            'warning',
+          );
+        } else {
+          if (this.metamask.netId === 56 || this.metamask.netId === 97) {
+            this.$refs.btnToggle.innerText = 'Connected';
+            this.$refs.btnToggle.className = 'btn btn-success';
+          } else {
+            this.makeToast(
+              'Warning',
+              `Your MetaMask in on the wrong network. Please switch on ${this.network.current.name} and try again!`,
+              'warning',
+            );
+            this.$refs.btnToggle.innerText = 'Connect';
+            this.$refs.btnToggle.className = 'btn btn-outline-warning';
+          }
+        }
+      },
+
+    },
+
   };
 </script>
