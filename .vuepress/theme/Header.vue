251f69918d19212e5d6a5f8699a863b6fdb92501
bit-business <101217799+bit-business@users.noreply.github.com>
2022-04-11 14:55:42 +0200

    1

--- .vuepress/theme/Header.vue
+++ .vuepress/theme/Header.vue
@@ -6,11 +6,14 @@
 <b-icon-menu-button-wide-fill></b-icon-menu-button-wide-fill></button>
         </b-navbar-brand>
 
-            <b-navbar-nav class="mx-auto">
+       <b-navbar-nav class="ml-auto" style="overflow-x: auto;">>
                 <b-nav-item to="/create-token/" :active="page === 'Generator'"><button type="button" class="btn btn-outline-secondary"><img height="21" width="75" src="/assets/images/bsc.png"></button></b-nav-item>
                 <b-nav-item target="_self" href="https://polygon.crypto-studio.net/"><button type="button" class="btn btn-outline-secondary"><img height="21" width="75" src="/assets/images/polygon.png"></button></b-nav-item>
                 <b-nav-item target="_self" @click="$bvToast.show('my-toast')"><button type="button" class="btn btn-outline-secondary"><img height="21" width="75" src="/assets/images/ethereumgray.png"></button></b-nav-item>
+<b-nav-item> <button type="button" class="btn btn-outline-warning" @click="connectmetamaskbutton" ref="btnToggle" >
 
+                                    Connect</button>
+  </b-nav-item>
                 <b-toast id="my-toast" variant="info" solid>
       <template #toast-title>
         <div class="d-flex flex-grow-1 align-items-baseline">
@@ -20,11 +23,7 @@
       </template>
      Creator for ETHEREUM network is nearly finished. If you need it, please contact us by contact form and we will send you a link to BETA version.
     </b-toast>
-       </b-navbar-nav>  <b-navbar-nav>
-<b-nav-item> <button type="button" class="btn btn-outline-warning" @click="connectmetamaskbutton" ref="btnToggle" >
-
-                                    Connect</button>
-  </b-nav-item> </b-navbar-nav>
+       </b-navbar-nav>
     </b-navbar>
 </template>
 
