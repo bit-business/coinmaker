94b9ed9f1bf70ef35b601a54157125341d8c3b9e
bit-business <101217799+bit-business@users.noreply.github.com>
2022-09-03 05:28:29 +0200

    dodaci

--- .vuepress/components/Generator.vue
+++ .vuepress/components/Generator.vue
@@ -447,7 +447,19 @@
         </b-row>
     </div>
 </template>
-
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/629755427b967b1179925980/1g4fia2se';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+  </script>
+  <!--End of Tawk.to Script-->
  <!--Start of Tawk.to Script-->
  <script lang="ts">
  // import WalletConnectProvider from '@walletconnect/web3-provider';
